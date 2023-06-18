47111f0ee51e88dbffb560ac757dd357366796d4
Ekeshwar <ekeshwiki@gmail.com>
2023-06-18 20:07:11 +0530

    minor changes

--- Round2/script.js
+++ Round2/script.js
@@ -1,5 +1,21 @@
-const imageElement = document.querySelector(".media-buttons img");
+const imageElement = document.querySelector("#play-button");
 
 imageElement.addEventListener("click", function() {
   imageElement.src = "Images/Pause.png";
 });
+// const imageElement = document.querySelector(".play-button");
+
+// const playImage = "Images/Play.png";
+// const pauseImage = "Images/Pause.png";
+
+// imageElement.src = playImage;
+
+// imageElement.addEventListener("click", () => {
+
+//   if (imageElement.src === playImage) {
+//     imageElement.src = pauseImage;
+//   } else {
+//     imageElement.src = playImage;
+//   }
+
+// });
