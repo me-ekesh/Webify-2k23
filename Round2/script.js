f1b320de7854abc3042d796c2ec930a1a87b485c
Ekeshwar <ekeshwiki@gmail.com>
2023-06-19 01:34:07 +0530

    added play/pause Toggle

--- Round2/script.js
+++ Round2/script.js
@@ -1,9 +1,9 @@
-const imageElement = document.querySelector("#play-button");
+// const imageElement = document.querySelector("#play-button");
 
-imageElement.addEventListener("click", function() {
-  imageElement.src = "Images/Pause.png";
-});
-// const imageElement = document.querySelector(".play-button");
+// imageElement.addEventListener("click", function() {
+//   imageElement.src = "Images/Pause.png";
+// });
+// const imageElement = document.querySelector("#play-button");
 
 // const playImage = "Images/Play.png";
 // const pauseImage = "Images/Pause.png";
@@ -19,3 +19,20 @@ imageElement.addEventListener("click", function() {
 //   }
 
 // });
+
+// Get the element by its ID
+const toggleButton = document.getElementById('play-button');
+
+// Set initial state and image source
+let isPlaying = false;
+let imageSrc = 'Images/Play.png';
+
+// Add click event listener
+toggleButton.addEventListener('click', function() {
+  // Toggle the state and update the image source
+  isPlaying = !isPlaying;
+  imageSrc = isPlaying ? 'Images/Pause.png' : 'Images/Play.png';
+  
+  // Update the image source
+  toggleButton.src = imageSrc;
+});
